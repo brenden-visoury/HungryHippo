083fda6fbdd09372b00716e5914c10ed24ad3dce
brenden-visoury <brenden.visoury@curvature.com>
2021-03-31 19:29:12 -0400

    no good

---
+++ sqlite_data_base.py
@@ -0,0 +1,9 @@
+# Create zillow database
+
+#Database Schema
+    # 28201 & 28217 (1 Mile Radius)
+    # Property Elements:
+        # Property Values
+            # Proper ID / City / Zip Code / Address /  Property Price /  Estimate Price
+            # Property Elements
+                # Acreage / Square Feet / Bedroom / Bathrooms / Other Elements (Hardwood, Carpet, Neighborhood)
--- zillow_data_ingestion.py
+++ zillow_data_ingestion.py
@@ -1,2 +1,12 @@
 # data ingestion using api
-# resources - pyzillow / xml parser
+# resources - pyzillow / xml parser /
+
+# pull data from zillow for a specific zip code:
+    # 28201 & 28217 (1 Mile Radius)
+        # Property Elements:
+            # Property Values
+                # Proper ID / State / City / Zip Code / Address /  Property Price /  Zestimate Price
+            # Property Elements
+                # Property ID / Acreage / Square Feet / Bedroom / Bathrooms / Other Elements (Hardwood, Carpet, Neighborhood)
+
+
